9165fe04f602f2d797074ed597ebe90b2d6961a5
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2018-12-21 20:12:06 +0000

    Implement the variable leaf node class.

---
+++ C++/polish/VarNode.h
@@ -0,0 +1,23 @@
+// This class models the variable leaf node.
+
+#ifndef VARNODE_H
+#define VARNODE_H
+
+#include<map>
+#include "LeafNode.h"
+
+using namespace std;
+
+class VarNode: public LeafNode {
+	protected:
+		char name;
+		map<char, int> values;
+
+	public:
+		VarNode(char id, map<char, int> m): name(id), values(m) {}
+		virtual void print(ostream& out) { out << name; }
+		virtual int eval(void) { return(values[name]); }
+		friend class Tree;
+};
+
+#endif //VARNODE_H
