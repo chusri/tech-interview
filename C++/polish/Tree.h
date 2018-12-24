693a0e050d8f4de6998068147119a747c8577c18
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2018-12-24 02:24:06 +0000

    Implement the Tree constructors.

--- C++/polish/Tree.h
+++ C++/polish/Tree.h
@@ -4,6 +4,10 @@
 #define TREE_H
 
 #include "Node.h"
+#include "IntNode.h"
+#include "VarNode.h"
+#include "UnaryNode.h"
+#include "BinaryNode.h"
 
 class Tree {
 	protected:
@@ -11,9 +15,9 @@ class Tree {
 
 	public:
 		Tree(int);
-		Tree(char);
-		Tree(char*, Tree);
-		Tree(char*, Tree, Tree);
+		Tree(char, map<char, int>);
+		Tree(char, Tree);
+		Tree(char, Tree, Tree);
 		Tree(const Tree&);
 		void operator=(const Tree&);
 		int eval(void);
@@ -21,6 +25,26 @@ class Tree {
 		friend class Node;
 		friend ostream& operator<<(ostream&, const Tree&);
 };
+
+// Integer leaf node
+Tree(int n) {
+	p = new IntNode(n);
+}
+
+// Variable leaf node
+Tree(char id, map<char, int> m) {
+	p = new VarNode(id, m);
+}
+
+// Unary node
+Tree(char op, Tree opnd) {
+	p = new UnaryNode(op, opnd);
+}
+
+// Binary tree
+Tree(char op, Tree left, Tree right) {
+	p = new BinaryNode(op, left, right);
+}
 
 // Copy constructor
 Tree::Tree(const Tree& t) {
@@ -44,6 +68,12 @@ Tree::~Tree(void) {
 ostream& operator<<(ostream& out, const Tree& t) {
 	t.p->print(out);
 	return(out);
+}
+
+// Overload the = operator
+void Tree::operator=(const Tree& t) {
+	p = t.p;
+	++p->gc;
 }
 
 #endif //TREE_H
