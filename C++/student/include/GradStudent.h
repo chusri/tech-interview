fa20b049c0a59daab549a20fa712f4a2d1e079e5
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2018-12-29 17:24:25 +0000

    Add doxygen tags in GradStudent.h

--- C++/student/include/GradStudent.h
+++ C++/student/include/GradStudent.h
@@ -1,4 +1,8 @@
-// This class models a graduate student at a university.
+/**
+ * @file GradStudent.h
+ * @brief This file implements the GradStudent class.
+ * @author Nawab Ali
+ */
 
 #ifndef GRADSTUDENT_H
 #define GRADSTUDENT_H
@@ -7,6 +11,11 @@
 
 enum class support{TA, RA, FELLOW, OTHER};
 
+/**
+ * @class GradStudent
+ * @brief This class creates and maintains records for a graduate student at
+ * a university.
+ */
 class GradStudent: public Student {
 	protected:
 		support s;
@@ -14,18 +23,70 @@ class GradStudent: public Student {
 		string thesis;
 
 	public:
+		/**
+		 * @brief Constructor for the GradStudent class.
+		 * @param id The student identification number.
+		 * @param name The name of the student (first last).
+		 * @param gpa The student's GPA.
+		 * @param y The year the student is enrolled in.
+		 * @param s The financial support that the student receives.
+		 * @param dept The student's academic dept.
+		 * @param thesis The student's thesis title.
+		 * @return None Constructor does not return anything.
+		 */
 		GradStudent(long id, string name, double gpa, year y, support s,
 								string dept, string thesis);
+
+		/**
+		 * @brief Getter method for student's financial support.
+		 * @param void
+		 * @return Student's financial support.
+		 */
 		support get_support(void) const;
+
+		/**
+		 * @brief Getter method for student's academic dept.
+		 * @param void
+		 * @return Student's academic dept.
+		 */
 		string get_dept(void) const;
+
+		/**
+		 * @brief Getter method for student's thesis title.
+		 * @param void
+		 * @return Student's thesis title.
+		 */
 		string get_thesis(void) const;
+
+		/**
+		 * @brief Setter method for student's financial support.
+		 * @param s Student's financial support.
+		 * @return void
+		 */
 		void set_support(const support s);
+
+		/**
+		 * @brief Setter method for student's academic dept.
+		 * @param dept Student's academic dept.
+		 * @return void
+		 */
 		void set_dept(const string dept);
+
+		/**
+		 * @brief Setter method for student's thesis title.
+		 * @param thesis Student's thesis title.
+		 * @return void
+		 */
 		void set_thesis(const string thesis);
+
+		/**
+		 * @brief Virtual function to print graduate student record.
+		 * @param out Reference to ostream object.
+		 * @return Reference to ostream object.
+		 */
 		ostream& print(ostream& out) const;
 };
 
-// Constructor
 GradStudent::GradStudent(long id, string name, double gpa, year y, support s,
 												 string dept, string thesis):
 												 Student(id, name, gpa, y),
@@ -56,7 +117,6 @@ void GradStudent::set_thesis(const string thesis) {
 	this->thesis = thesis;
 }
 
-// Print the member variables
 ostream& GradStudent::print(ostream& out) const {
 	Student::print(out);
 	out << ",";
