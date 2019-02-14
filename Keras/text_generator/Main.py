8b0649af778606a7a0e660cb26e1601698c1e436
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-02-14 20:44:43 +0000

    1. Tokenize training data. 2. Make helper functions private.

--- Keras/text_generator/Main.py
+++ Keras/text_generator/Main.py
@@ -14,8 +14,14 @@ def main():
     """
 
     text_generator = TextGenerator('republic.txt')
-    training_data = text_generator.load_training_data()
+
+    training_data = text_generator._load_training_data()
     print training_data[:200]
+
+    tokens = text_generator._tokenize_training_data(training_data)
+    print tokens[:200]
+    print 'Total tokens: %d' % len(tokens)
+    print 'Unique tokens: %d' % len(set(tokens))
 
 if __name__ == '__main__':
     main()
--- Keras/text_generator/TextGenerator.py
+++ Keras/text_generator/TextGenerator.py
@@ -1,5 +1,7 @@
 """ Generate text sequences using a statistical language model. """
 
+import string
+
 class TextGenerator:
     def __init__(self, training_data_file):
         """
@@ -15,7 +17,7 @@ class TextGenerator:
 
         self.training_data_file = training_data_file
 
-    def load_training_data(self):
+    def _load_training_data(self):
         """
         Load training data to memory.
 
@@ -29,3 +31,35 @@ class TextGenerator:
         with open(self.training_data_file, 'r') as file:
             training_data = file.read()
         return training_data
+
+    def _tokenize_training_data(self, training_data):
+        """
+        Clean and tokenize training data.
+        1. Replace '-' with a white space so we can split words better
+        2. Split words based on white space
+        3. Remove all punctuation from words to reduce the vocabulary size
+           (e.g. 'What?' becomes 'What')
+        4. Remove all words that are not alphabetic to remove standalone
+           punctuation tokens
+        5. Normalize all words to lowercase to reduce the vocabulary size
+
+        Arguments:
+        self
+
+        Returns:
+        tokens -- tokenized data
+        """
+
+        training_data = training_data.replace('--', ' ')
+        tokens = training_data.split()
+
+        # Remove all punctuation from words to reduce the vocabulary size
+        table = string.maketrans('', '')
+        tokens = [word.translate(table, string.punctuation) for word in tokens]
+
+        # Remove tokens that are not alphabetic
+        tokens = [word for word in tokens if word.isalpha()]
+
+        tokens = [word.lower() for word in tokens]
+        return tokens
+
