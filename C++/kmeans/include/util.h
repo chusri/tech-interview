4a95286f56b2b7e3081bf10a75f8713e525e81ba
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-02-24 02:50:27 +0000

    Refactor utility code.

--- C++/kmeans/include/util.h
+++ C++/kmeans/include/util.h
@@ -1,6 +1,6 @@
 /**
  * @file util.h
- * @brief This file declares common utility functions.
+ * @brief This file implements common utility functions.
  * @author Nawab Ali
  */
 
@@ -9,6 +9,8 @@
 
 #include<string>
 #include<vector>
+#include<fstream>
+#include<iostream>
 
 using namespace std;
 
@@ -20,6 +22,18 @@ using namespace std;
  */
 template <class T>
 vector<vector<T> >
-read_training_data(string training_data_file, int num_features);
+read_training_data(string training_data_file, int num_features) {
+	string line;
+	ifstream file;
+	vector<vector<T> > training_data;
+
+	file.open(training_data_file);
+	while (getline(file, line)) {
+		cout << line << endl;
+	}
+	file.close();
+
+	return(training_data);
+}
 
 #endif //UTIL_H
--- C++/kmeans/src/kmeans.cpp
+++ C++/kmeans/src/kmeans.cpp
@@ -5,24 +5,6 @@
  */
 
 #include "util.h"
-#include<fstream>
-#include<iostream>
-
-template <class T>
-vector<vector<T> >
-read_training_data(string training_data_file, int num_features) {
-	string line;
-	ifstream file;
-	vector<vector<T> > training_data;
-
-	file.open(training_data_file);
-	while (getline(file, line)) {
-		cout << line << endl;
-	}
-	file.close();
-
-	return(training_data);
-}
 
 int main(int argc, char** argv) {
 	read_training_data<int>("../data/iris.txt", 4);
