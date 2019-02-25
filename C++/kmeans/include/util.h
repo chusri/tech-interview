099d38f60f9a4d01c4c2259376c567e8dbb4cbe1
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-02-25 01:01:05 +0000

    1. Rename kmeans.cpp to main.cpp 2. Read training data from file and convert to 2D vector

--- C++/kmeans/include/util.h
+++ C++/kmeans/include/util.h
@@ -10,12 +10,38 @@
 #include<string>
 #include<vector>
 #include<fstream>
+#include<sstream>
 #include<iostream>
+#include<boost/lexical_cast.hpp>
+#include<boost/algorithm/string.hpp>
 
 using namespace std;
 
 /**
- * @brief This function reads training data from a file.
+ * @brief This function splits a string into tokens.
+ * @param delimiter Delimiter character.
+ * @param num_features Number of features in the data.
+ * @return Vector containing tokenized string.
+ */
+template <class T>
+vector<T> tokenize(const string& str, char delimiter, int num_features) {
+	string token;
+	vector<T> tokens;
+	int token_count = 0;
+	istringstream token_stream(str);
+
+	// Convert str to tokens. Only process the first num_features tokens.
+	while (getline(token_stream, token, delimiter) && token_count < num_features) {
+		boost::trim(token);
+		tokens.push_back(boost::lexical_cast<T>(token));
+		token_count++;
+	}
+
+	return(tokens);
+}
+
+/**
+ * @brief This function reads training data from a csv file.
  * @param training_data_file Training data file.
  * @param num_features Number of features in the data.
  * @return 2-dimensional vector with the training data.
@@ -28,11 +54,12 @@ read_training_data(string training_data_file, int num_features) {
 	vector<vector<T> > training_data;
 
 	file.open(training_data_file);
-	while (getline(file, line)) {
-		cout << line << endl;
-	}
-	file.close();
 
+	while (getline(file, line)) {
+		training_data.push_back(tokenize<T>(line, ',', num_features));
+	}
+
+	file.close();
 	return(training_data);
 }
 
---
+++ C++/kmeans/src/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file main.cpp
+ * @brief This file implements the main thread of execution.
+ * @author Nawab Ali
+ */
+
+#include "util.h"
+#include<algorithm>
+
+void print_element(double d) {
+	cout << d << " ";
+}
+
+void print_vector(vector<double> v) {
+	for_each(v.begin(), v.end(), print_element);
+	cout << endl;
+}
+
+int main(int argc, char** argv) {
+	vector<vector<double> > training_data;
+
+	training_data = read_training_data<double>("../data/iris.txt", 4);
+
+	// Print the training data
+	for_each(training_data.begin(), training_data.end(), print_vector);
+	return(0);
+}
