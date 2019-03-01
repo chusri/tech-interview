6b6deaea3c21b738d03d815e133cdc2428527b58
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-03-01 01:29:23 +0000

    Add 2 unit tests to read_training_data() test suite.

--- C++/kmeans/src/unit_tests.cpp
+++ C++/kmeans/src/unit_tests.cpp
@@ -7,11 +7,19 @@
 #include "util.h"
 #include<gtest/gtest.h>
 
-TEST (KMeansTest, VectorSize) {
+TEST (KMeansTest, ReadTrainingData) {
+	vector<double> row0, row149;
 	vector<vector<double> > training_data;
+	vector<double> row0_ground_truth{5.1,3.5,1.4,0.2};
+	vector<double> row149_ground_truth{5.9,3.0,5.1,1.8};
+
   training_data = read_training_data<double>("../data/iris.txt", 4);
+	row0 = training_data[0];
+	row149 = training_data[149];
 
 	ASSERT_EQ(150, training_data.size());
+	ASSERT_EQ(row0, row0_ground_truth);
+	ASSERT_EQ(row149, row149_ground_truth);
 }
 
 int main(int argc, char** argv) {
