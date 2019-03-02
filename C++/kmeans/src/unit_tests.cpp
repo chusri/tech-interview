f16401160301bde35d776dacc30fa2451ed5c5ac
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-03-02 16:12:25 +0000

    Add unit tests for tokenize() function.

--- C++/kmeans/src/unit_tests.cpp
+++ C++/kmeans/src/unit_tests.cpp
@@ -7,6 +7,27 @@
 #include "util.h"
 #include<gtest/gtest.h>
 
+TEST (KMeansTest, TokenizeData) {
+	vector<double> row0, row149;
+	vector<double> row0_1, row0_2, row0_3;
+	vector<double> row0_1_ground_truth{5.1,3.5,1.4};
+	vector<double> row0_ground_truth{5.1,3.5,1.4,0.2};
+	vector<double> row149_ground_truth{5.9,3.0,5.1,1.8};
+
+	row0 = tokenize<double>("5.1,3.5,1.4,0.2,Iris-setosa", ',', 4);
+	row0_1 = tokenize<double>("5.1,3.5,1.4,0.2,Iris-setosa", ',', 3);
+	row0_2 = tokenize<double>("5.1,3.5,1.4,0.2,Iris-setosa", ',', 0);
+	row0_3 = tokenize<double>("5.1,3.5,1.4,0.2,Iris-setosa", ',', -1);
+	row149 = tokenize<double>("5.9,3.0,5.1,1.8,Iris-virginica", ',', 4);
+
+	ASSERT_EQ(row0, row0_ground_truth);
+	ASSERT_NE(row0, row149_ground_truth);
+	ASSERT_EQ(row0_1, row0_1_ground_truth);
+	ASSERT_EQ(row0_2.size(), 0);
+	ASSERT_EQ(row0_3.size(), 0);
+	ASSERT_EQ(row149, row149_ground_truth);
+}
+
 TEST (KMeansTest, ReadTrainingData) {
 	vector<double> row0, row149;
 	vector<vector<double> > training_data;
@@ -20,6 +41,7 @@ TEST (KMeansTest, ReadTrainingData) {
 	ASSERT_EQ(150, training_data.size());
 	ASSERT_EQ(row0, row0_ground_truth);
 	ASSERT_EQ(row149, row149_ground_truth);
+	ASSERT_NE(row0, row149_ground_truth);
 }
 
 int main(int argc, char** argv) {
