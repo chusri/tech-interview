e6564a0d2fedb73d421bf9838dbbbbb9e390a365
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-03-23 01:04:35 +0000

    Refactor unit tests for read_training_data utility function.

--- C++/kmeans/src/unit_tests.cpp
+++ C++/kmeans/src/unit_tests.cpp
@@ -45,20 +45,31 @@ TEST (KMeansTest, UtilTokenizeDataDimNegative1) {
 	ASSERT_EQ(tokens.size(), 0);
 }
 
-TEST (KMeansTest, ReadTrainingData) {
-	vector<double> row0, row149;
+// Unit tests for read_training_data utility function
+TEST (KMeansTest, UtilReadTrainingDataSize) {
 	vector<vector<double> > training_data;
-	vector<double> row0_ground_truth{5.1,3.5,1.4,0.2};
-	vector<double> row149_ground_truth{5.9,3.0,5.1,1.8};
 
   training_data = read_training_data<double>("../data/iris.txt", 4);
-	row0 = training_data[0];
-	row149 = training_data[149];
 
 	ASSERT_EQ(150, training_data.size());
-	ASSERT_EQ(row0, row0_ground_truth);
-	ASSERT_EQ(row149, row149_ground_truth);
-	ASSERT_NE(row0, row149_ground_truth);
+}
+
+TEST (KMeansTest, UtilReadTrainingDataFirstRow) {
+	vector<double> row0{5.1,3.5,1.4,0.2};
+	vector<vector<double> > training_data;
+
+  training_data = read_training_data<double>("../data/iris.txt", 4);
+
+	ASSERT_EQ(row0, training_data[0]);
+}
+
+TEST (KMeansTest, UtilReadTrainingDataLastRow) {
+	vector<vector<double> > training_data;
+	vector<double> row149{5.9,3.0,5.1,1.8};
+
+  training_data = read_training_data<double>("../data/iris.txt", 4);
+
+	ASSERT_EQ(row149, training_data[149]);
 }
 
 TEST (KMeansTest, GetColumnFrom2Dvec) {
