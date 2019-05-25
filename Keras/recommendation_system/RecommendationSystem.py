9d93b5b747e1c9a6b2a2c8734f94517c849ec5b7
nawab-ali <ubuntu@ip-172-31-37-159.us-west-2.compute.internal>
2019-05-25 01:05:00 +0000

    1. Preprocess training data 2. Rename Movielens dataset file 3. Add unit tests

--- Keras/recommendation_system/RecommendationSystem.py
+++ Keras/recommendation_system/RecommendationSystem.py
@@ -1,9 +1,12 @@
 #!/home/ubuntu/anaconda3/envs/tensorflow_p36/bin/python
 
-""" Deep Learning based Recommendation System for movies, books etc. """
+""" Neural Collaborative Filtering based Recommendation System for movies, books etc. """
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 class RecommendationSystem:
-    def __init__(self, training_data_file=None, trained_model_file=None):
+    def __init__(self, training_data_file, trained_model_file):
         """
         Initialize Recommendation System object.
 
@@ -15,8 +18,31 @@ class RecommendationSystem:
         Returns:
         None
         """
-        self.training_data_file = training_data_file
+
+        self.train_data, self.test_data = self._preprocess_training_data(training_data_file)
         self.trained_model_file = trained_model_file
+
+    def _preprocess_training_data(self, training_data_file):
+        """
+        Read the training dataset from file and preprocess it.
+
+        Arguments:
+        self
+        training_data_file -- file containing training data
+
+        Returns:
+        Pandas dataframes containing train and test datasets
+        """
+
+        dataset = pd.read_csv(training_data_file, sep='\t',
+                              names='user_id,item_id,rating,timestamp'.split(','))
+
+        # Assign a unique user_id between (0, #users)
+        dataset.user_id = dataset.user_id.astype('category').cat.codes.values
+        # Assign a unique item_id between (0, #items)
+        dataset.item_id = dataset.item_id.astype('category').cat.codes.values
+
+        return train_test_split(dataset, test_size=0.2)
 
 def main():
     """
---
+++ Keras/recommendation_system/unit_test.py
@@ -0,0 +1,16 @@
+#!/home/ubuntu/anaconda3/envs/tensorflow_p36/bin/python
+
+import unittest
+from RecommendationSystem import *
+
+class TestRecommendationSystem(unittest.TestCase):
+    def test_preprocess_training_data_train_data_shape(self):
+        recommendation_system = RecommendationSystem('data/movielens_100k.data', None)
+        self.assertEqual(recommendation_system.train_data.shape, (80000, 4))
+
+    def test_preprocess_training_data_test_data_shape(self):
+        recommendation_system = RecommendationSystem('data/movielens_100k.data', None)
+        self.assertEqual(recommendation_system.test_data.shape, (20000, 4))
+
+if __name__ == '__main__':
+    unittest.main()
