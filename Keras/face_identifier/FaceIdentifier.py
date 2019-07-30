acb5657f0781ff0e19a475774d4da3ae5a74e91c
nawab-ali <ubuntu@ip-172-31-37-159.us-west-2.compute.internal>
2019-07-30 17:52:17 +0000

    Add FaceNet model and celebrity data set.

---
+++ Keras/face_identifier/FaceIdentifier.py
@@ -0,0 +1,7 @@
+#!/home/ubuntu/anaconda3/envs/tensorflow_p36/bin/python
+
+""" Deep Learning system for identifying faces. """
+
+class FaceIdentifier(Object):
+    def __init__(self):
+        pass
