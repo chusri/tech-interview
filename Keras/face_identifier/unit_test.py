cb72d8d7bbfa691ac56b36663fe9a58b2179a9c6
nawab-ali <ubuntu@ip-172-31-37-159.us-west-2.compute.internal>
2019-07-31 01:12:39 +0000

    Ignore warnings.

--- Keras/face_identifier/unit_test.py
+++ Keras/face_identifier/unit_test.py
@@ -1,10 +1,12 @@
 #!/home/ubuntu/anaconda3/envs/tensorflow_p36/bin/python
 
+import warnings
 import unittest
 from FaceIdentifier import *
 
 class TestFaceIdentifier(unittest.TestCase):
     def test_load_facenet_model(self):
+        warnings.filterwarnings('ignore')
         face_identifier = FaceIdentifier('data/facenet_keras.h5')
         model = face_identifier._load_facenet_model()
         print(model.inputs)
@@ -17,4 +19,4 @@ class TestFaceIdentifier(unittest.TestCase):
         self.assertEqual(pixels.shape, (264, 200, 3))
 
 if __name__ == '__main__':
-   unittest.main()
+    unittest.main()
