03cab5ca145e3953409d4c69d4b9f35dd3eec7dc
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-09-09 00:18:06 +0000

    Implement main() and unit_test skeleton.

---
+++ C++/employee/src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @brief This file implements the main thread of execution.
+ * @author Nawab Ali
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	return(0);
+}
---
+++ C++/employee/src/unit_tests.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file unit_tests.cpp
+ * @brief This file implements the unit tests.
+ * @author Nawab Ali
+ */
+
+#include<gtest/gtest.h>
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return(RUN_ALL_TESTS());
+}
