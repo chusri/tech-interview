f33282d72120f54ff4393e410e2c8ba8b45d24be
nawab-ali <ubuntu@ip-172-31-27-183.us-west-2.compute.internal>
2019-09-09 18:58:28 +0000

    Write output to file.

--- Python/word_count/wc.py
+++ Python/word_count/wc.py
@@ -8,11 +8,12 @@ def main():
     num_words = 0
     num_chars = 0
 
-    if len(sys.argv) < 2:
-        print 'Usage: ' + sys.argv[0] + ' <filename>'
+    if len(sys.argv) < 3:
+        print 'Usage:', sys.argv[0], '<input_file>', '<output_file>'
         exit(1)
     else:
         input_file_name = sys.argv[1]
+        output_file_name = sys.argv[2]
 
     with open(input_file_name, 'r') as input_file_handle:
         lines = [line for line in input_file_handle]
@@ -20,6 +21,9 @@ def main():
     for line in lines:
         num_words += count_words(line)
         num_chars += count_characters(line)
+
+    with open(output_file_name, 'w') as output_file_handle:
+        output_file_handle.write(str(len(lines)) + ' ' + str(num_words) + ' ' + str(num_chars))
 
     print len(lines), num_words, num_chars
 
